e413c40fb2ca41c82fa24e0198fb03067d930b3d
Pradeep K <PRADEEP.DECE@MSN.COM>
2023-12-16 10:21:35 +0530

    changing data

---
+++ Testing Examples using pytest/Example 1/mathlib.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 16 09:44:49 2023
+
+@author: prade
+"""
+
+def calc_addition(a, b):
+    return a + b
+ 
+ 
+def calc_multiply(a, b):
+    return a * b
+     
+ 
+def calc_substraction(a, b):
+    return a - b
